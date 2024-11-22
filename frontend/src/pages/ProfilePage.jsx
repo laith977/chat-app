72b40eb96a1397cfe7b21e3a87f548aac8f8cc8e
laith977 <lith.adnan@yahoo.com>
2024-11-22 15:05:06 +0300

    Signup & Login Pages

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import { useAuthStore } from "../store/useAuthStore";
+
+const ProfilePage = () => {
+  const { authUser } = useAuthStore();
+
+  return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
