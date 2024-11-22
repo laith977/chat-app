e4f09b0c3540638a49050045224d1150dd100271
laith977 <lith.adnan@yahoo.com>
2024-11-22 23:21:21 +0300

    Home Page and ChatContainer

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -36,7 +36,7 @@ export const sendMessage = async (req, res) => {
   try {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
-    const sendderId = req.user._id;
+    const senderId = req.user._id;
 
     let imageUrl;
     if (image) {
